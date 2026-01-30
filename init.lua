2ba39e9042e065130a4dbec366ce14eb619560a5
Gregor Hartmann <gregor@hartmann.pw>
2026-01-30 10:05:13 +0100

    add noctalia theming integration

--- init.lua
+++ init.lua
@@ -969,7 +969,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -996,6 +996,8 @@ require('lazy').setup({
     },
   },
 })
+
+require('matugen').setup()
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
